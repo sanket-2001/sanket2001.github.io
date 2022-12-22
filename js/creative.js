3a53dfaa13a566c0b5063cb6a270adf10c700d66
Sanket <81515371+sanket-2001@users.noreply.github.com>
2022-12-22 18:21:11 +0530

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -1,8 +1,8 @@
 
     (function($) {
-  "use strict"; // Start of use strict
+  "use strict"; 
 
-  // Smooth scrolling using jQuery easing
+
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -16,18 +16,17 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
+
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
     offset: 75
   });
 
-  // Collapse Navbar
+
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-scrolled");
@@ -35,12 +34,12 @@
       $("#mainNav").removeClass("navbar-scrolled");
     }
   };
-  // Collapse now if page is not at top
+ 
   navbarCollapse();
-  // Collapse the navbar when page is scrolled
+ 
   $(window).scroll(navbarCollapse);
 
-  // Magnific popup calls
+
   $('#portfolio').magnificPopup({
     delegate: 'a',
     type: 'image',
